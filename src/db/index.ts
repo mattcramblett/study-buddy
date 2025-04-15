5e788cbb2471a093b2d8e65ab01dc0426a121427
Matt Cramblett <Cramblett5@gmail.com>
2025-04-14 23:05:16 -0400

    start setting up nextauth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,11 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: 'sqlite',
+  schema: './src/db/schema.ts'
+})
 
 export const db = drizzle({
   connection: {
