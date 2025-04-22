1a0bfd797d4d10d3d36f1a8199c8254384cc815b
Matt Cramblett <Cramblett5@gmail.com>
2025-04-21 21:24:31 -0400

    fix some schema stuff

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,15 +18,15 @@ export const flashcards = t.sqliteTable(
   "flashcards",
   {
     id: t.integer("id").primaryKey({ autoIncrement: true }).notNull(),
-    flashcard_set_id: t.integer("flashcard_set_id")
+    flashcardSetId: t.integer("flashcard_set_id")
       .notNull()
       .references(() => flashcard_sets.id),
-    front_side: t.text("front_side").notNull(),
-    back_side: t.text("back_side").notNull(),
-    created_at: t.integer("created_at", { mode: "timestamp_ms" }).notNull().defaultNow(),
+    frontSide: t.text("front_side").notNull(),
+    backSide: t.text("back_side").notNull(),
+    createdAt: t.integer("created_at", { mode: "timestamp_ms" }).notNull().defaultNow(),
   },
   (table) => [
-    t.index("flashcards_id_idx").on(table.flashcard_set_id),
+    t.index("flashcard_set_id_idx").on(table.flashcardSetId),
   ],
 );
 
@@ -35,12 +35,14 @@ export const flashcard_sets = t.sqliteTable(
   {
     id: t.integer("id").primaryKey({ autoIncrement: true }).notNull(),
     uuid: t.text("uuid", { length: 36 }).unique().notNull(),
+    userId: t.integer("user_id").notNull().references(() => users.id),
     name: t.text("name", { length: 256 }).notNull(),
-    created_at: t.integer("created_at", { mode: "timestamp_ms" }).notNull().defaultNow(),
+    createdAt: t.integer("created_at", { mode: "timestamp_ms" }).notNull().defaultNow(),
   },
   (table) => [
     t.uniqueIndex("flashcard_sets_uuid_idx").on(table.uuid),
     t.index("flashcard_sets_name_idx").on(table.name),
+    t.index("flashcard_sets_user_id_idx").on(table.userId),
   ],
 );
 
